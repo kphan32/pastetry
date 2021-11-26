df5cfe8593da0e4d04390c35e05dd10f089d46d2
Kavin Phan <kavin@kphan.tech>
2021-11-26 11:44:53 -0500

    Fix flex in Fullscreen comp

--- src/screens/AppContainer/components/Fullscreen.tsx
+++ src/screens/AppContainer/components/Fullscreen.tsx
@@ -3,7 +3,7 @@ interface FullscreenProps {
 }
 
 const Fullscreen: React.FC<FullscreenProps> = ({ children }) => {
-  return <div className="min-h-screen">{children}</div>;
+  return <div className="min-h-screen flex">{children}</div>;
 };
 
 export default Fullscreen;
