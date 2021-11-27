b490e8224acbee2b9646c7776c763c05448f3eff
Kavin Phan <kavin@kphan.tech>
2021-11-27 15:40:47 -0500

    Add instructions for tablet size

--- src/screens/Home/components/Instructions.tsx
+++ src/screens/Home/components/Instructions.tsx
@@ -1,6 +1,6 @@
 const Instructions = () => {
   return (
-    <div className="mt-3 md:mt-5 lg:mt-7 hidden lg:block">
+    <div className="mt-3 md:mt-5 lg:mt-7 hidden md:block">
       <p className="text-sm md:text-xl lg:text-2xl">
         Create a paste by hitting{" "}
         <code className="rounded-md bg-gray-100 p-1 md:mg-1">CTRL + V</code>
--- src/screens/Home/components/PasteButton.tsx
+++ src/screens/Home/components/PasteButton.tsx
@@ -6,11 +6,11 @@ const PasteButton: React.FC<Props> = ({ onClick }) => {
   return (
     <div>
       <button
-        className="mt-2 rounded-md bg-blue-500 hover:bg-blue-600 px-3 py-2 text-white text-sm lg:mt-4 lg:text-lg lg:px-5 lg:py-2"
+        className="mt-4 rounded-md bg-blue-500 hover:bg-blue-600 px-3 py-2 text-white text-sm lg:mt-6 lg:text-lg lg:px-5 lg:py-2"
         onClick={onClick}
       >
-        <p className="lg:hidden">Create a Paste</p>
-        <p className="hidden lg:block">or Click Here</p>
+        <p className="md:hidden">Create a Paste</p>
+        <p className="hidden md:block">or Click Here</p>
       </button>
     </div>
   );
