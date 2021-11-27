91eb100701c5a1ed54bc400f4e0bfc125e5d2021
Kavin Phan <kavin@kphan.tech>
2021-11-26 20:14:40 -0500

    Add author to footer

--- src/screens/AppContainer/components/Footer.tsx
+++ src/screens/AppContainer/components/Footer.tsx
@@ -1,8 +1,17 @@
 const Footer = () => {
   return (
-    <footer className="flex items-center justify-center w-full h-24 border-t">
+    <footer className="flex items-center justify-center w-full h-24 border-t divide-x divide-black">
+      <p className="px-2">
+        Made by{" "}
+        <a
+          href="https://github.com/kphan32"
+          className="text-blue-500 hover:underline"
+        >
+          Kavin Phan
+        </a>
+      </p>
       <a
-        className="flex items-center justify-center"
+        className="flex items-center justify-center px-2"
         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
         target="_blank"
         rel="noopener noreferrer"
