824f1ac54bc4ae1ef23ae068a40ee9ba52d22bdc
Kavin Phan <kavin@kphan.tech>
2021-11-27 14:35:18 -0500

    Style paste content

--- src/screens/ViewPaste/index.tsx
+++ src/screens/ViewPaste/index.tsx
@@ -9,11 +9,12 @@ const ViewPaste: React.FC<Props> = ({ pasteContent }) => {
   console.info(pasteContent);
   return (
     <main className="flex flex-1 w-full">
-      <div className="m-2 p-2 flex-1 bg-gray-200 rounded-md shadow-inner">
+      <div className="m-2 p-2 flex-1 bg-gray-100 rounded-md shadow-inner">
         <code className="whitespace-pre-line h-full">
           <textarea
             value={pasteContent}
-            className="w-full h-full bg-gray-200 resize-none"
+            className="w-full h-full bg-gray-100 outline-none resize-none"
+            readOnly
           ></textarea>
         </code>
       </div>
