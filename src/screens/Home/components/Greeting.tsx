f381c046a7b1eac0763b3657d3e769892bcbe781
Kavin Phan <kavin@kphan.tech>
2021-11-27 15:38:23 -0500

    Make greeting bigger

--- src/screens/Home/components/Greeting.tsx
+++ src/screens/Home/components/Greeting.tsx
@@ -1,7 +1,7 @@
 const Greeting = () => {
   return (
     <div>
-      <h1 className="text-2xl font-bold md:text-4xl lg:text-6xl">
+      <h1 className="text-4xl font-bold md:text-5xl lg:text-6xl">
         Welcome to <p className="text-blue-600">Pastetry</p>
       </h1>
     </div>
