a420ce44b007dec9a3d4ae11cd0d6b626d612323
Kavin Phan <kavin@kphan.tech>
2021-12-08 18:51:11 -0500

    Fix broken padding on paste button

--- src/screens/Home/components/PasteButton.tsx
+++ src/screens/Home/components/PasteButton.tsx
@@ -6,7 +6,7 @@ const PasteButton: React.FC<PasteButtonProps> = ({ onClick }) => {
   return (
     <div>
       <button
-        className="mt-4 rounded-md bg-blue-500 hover:bg-blue-600 px-3 py- text-sm lg:mt-6 lg:text-lg lg:px-5 lg:py-2 transition-colors shadow-md"
+        className="mt-4 rounded-md bg-blue-500 hover:bg-blue-600 px-3 py-2 text-sm lg:mt-6 lg:text-lg lg:px-5 lg:py-2 transition-colors shadow-md"
         onClick={onClick}
       >
         <p className="text-white md:hidden">Create a Paste</p>
