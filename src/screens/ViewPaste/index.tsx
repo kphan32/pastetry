40ca291c111a8baa0d5f95cb713ee937a87c3c1d
Kavin Phan <kavin@kphan.tech>
2021-12-08 18:45:31 -0500

    Fix paste view height

--- src/screens/ViewPaste/index.tsx
+++ src/screens/ViewPaste/index.tsx
@@ -11,11 +11,11 @@ const ViewPaste: React.FC<Props> = ({ pasteContent }) => {
     <main className="flex flex-1 flex-col w-full">
       <Header />
 
-      <div className="m-2 p-2 flex-1 bg-gray-200 rounded-md shadow-inner">
-        <code className="whitespace-pre-line h-full">
+      <div className="flex flex-1 m-2 p-2 bg-gray-200 rounded-md shadow-inner">
+        <code className="flex flex-1 whitespace-pre-line">
           <textarea
             value={pasteContent}
-            className="w-full h-full bg-gray-200 outline-none resize-none"
+            className="flex-1 bg-gray-200 outline-none resize-none"
             readOnly
           ></textarea>
         </code>
