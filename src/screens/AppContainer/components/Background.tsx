24d00654317664574ba6cce950cf1f95244099c2
Kavin Phan <kavin@kphan.tech>
2021-12-08 18:22:35 -0500

    Fix background image padding

--- src/screens/AppContainer/components/Background.tsx
+++ src/screens/AppContainer/components/Background.tsx
@@ -2,7 +2,7 @@ import { GiCupcake } from "react-icons/gi";
 
 const Background = () => {
   return (
-    <div className="absolute -z-10 left-0 top-0 w-full h-full pt-8 pb-18 lg:pb-20">
+    <div className="absolute -z-10 left-0 top-0 w-full h-full pt-8 pb-16">
       <GiCupcake className="w-full h-full opacity-025" />
     </div>
   );
