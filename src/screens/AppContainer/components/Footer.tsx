90f85749ae48abdc8faa41d7c8f453bf92533ae0
Kavin Phan <kavin@kphan.tech>
2021-12-08 18:18:40 -0500

    Remove Vercel reference in Footer

--- src/screens/AppContainer/components/Footer.tsx
+++ src/screens/AppContainer/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Author = () => {
   return (
-    <p className="text-xs pr-2 md:text-md lg:text-lg">
+    <p className="text-xs md:text-md lg:text-lg">
       Made by{" "}
       <a
         href="https://github.com/kphan32/pastetry"
@@ -12,24 +12,10 @@ const Author = () => {
   );
 };
 
-const MadeByVercel = () => {
-  return (
-    <a
-      className="pl-2"
-      href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <img src="/vercel.svg" alt="Vercel Logo" className="h-3 lg:h-4" />
-    </a>
-  );
-};
-
 const Footer = () => {
   return (
-    <footer className="flex items-center justify-center w-full h-10 border-t divide-x divide-gray-300 lg:h-12">
+    <footer className="flex items-center justify-center w-full h-10 border-t lg:h-12">
       <Author />
-      <MadeByVercel />
     </footer>
   );
 };
